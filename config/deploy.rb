a3b4a72c306183f6afc852bf647677371f5b97e2
Max G <Lorjax@gmx.de>
2015-02-12 14:56:05 +0100

    test4

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,63 +1,43 @@
-# config valid only for current version of Capistrano
-lock '3.3.5'
+require 'bundler/capistrano'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@github.com:Lorjax/TestApp.git'
-set :deploy_to, '/home/deployer/TestApp'
+set :application, "TestApp"
+set :repository,  "git@github.com:Lorjax/TestApp.git"
+set :deploy_to, "/home/deployer/"
 set :scm, :git
 set :branch, "master"
 set :user, "deployer"
-set :use_sudo, "false"
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :use_sudo, false
 set :rails_env, "production"
 set :deploy_via, :copy
-set :keep_releases, 5
 set :ssh_options, { :forward_agent => true, :port => 2247 }
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-
-
-# namespace :deploy do
-
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
-
-# end
-
+set :keep_releases, 5
+default_run_options[:pty] = true
+server "example.com", :app, :web, :db, :primary => true
 
 namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join('tmp/restart.txt')
-    end
+  desc "Symlink shared config files"
+  task :symlink_config_files do
+    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
   end
 
-  after :publishing, 'deploy:restart'
-  after :finishing, 'deploy:cleanup'
+  # NOTE: I don't use this anymore, but this is how I used to do it.
+  desc "Precompile assets after deploy"
+  task :precompile_assets do
+    run <<-CMD
+      cd #{ current_path } &&
+      #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
+    CMD
+  end
+
+  desc "Restart applicaiton"
+  task :restart do
+    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+  end
 end
+
+after "deploy", "deploy:symlink_config_files"
+after "deploy", "deploy:restart"
+after "deploy", "deploy:cleanup"
