c066c174b592745766400b5b87462c8daa3077df
Max G <Lorjax@gmx.de>
2015-02-12 15:02:02 +0100

    test5

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,43 +1,20 @@
-require 'bundler/capistrano'
+set :application, 'TestApp'
+set :repo_url, 'git@github.com:Lorax/TestApp.git'
 
-set :application, "TestApp"
-set :repository,  "git@github.com:Lorjax/TestApp.git"
-set :deploy_to, "/home/deployer/"
-set :scm, :git
-set :branch, "master"
-set :user, "deployer"
-set :use_sudo, false
-set :rails_env, "production"
-set :deploy_via, :copy
-set :ssh_options, { :forward_agent => true, :port => 2247 }
-set :keep_releases, 5
-default_run_options[:pty] = true
-server "example.com", :app, :web, :db, :primary => true
+set :deploy_to, '/home/deployer/TestApp'
+
+set :linked_files, %w{config/database.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
 
-  desc "Symlink shared config files"
-  task :symlink_config_files do
-    run "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
-  end
-
-  # NOTE: I don't use this anymore, but this is how I used to do it.
-  desc "Precompile assets after deploy"
-  task :precompile_assets do
-    run <<-CMD
-      cd #{ current_path } &&
-      #{ sudo } bundle exec rake assets:precompile RAILS_ENV=#{ rails_env }
-    CMD
-  end
-
-  desc "Restart applicaiton"
+  desc 'Restart application'
   task :restart do
-    run "#{ try_sudo } touch #{ File.join(current_path, 'tmp', 'restart.txt') }"
+    on roles(:app), in: :sequence, wait: 5 do
+      execute :touch, release_path.join('tmp/restart.txt')
+    end
   end
-end
 
-after "deploy", "deploy:symlink_config_files"
-after "deploy", "deploy:restart"
-after "deploy", "deploy:cleanup"
+  after :publishing, 'deploy:restart'
+  after :finishing, 'deploy:cleanup'
+end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,20 +1,5 @@
 set :stage, :production
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary server in each group
-# is considered to be the first unless any hosts have the primary
-# property set.  Don't declare `role :all`, it's a meta role.
 
-# role :app, %w{deployer@192.168.0.112:2247}
-# role :web, %w{deployer@192.168.0.112:2247}
-# role :db,  %w{deployer@192.168.0.112:2247}
-
-
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server definition into the
-# server list. The second argument is a, or duck-types, Hash and is
-# used to set extended properties on the server.
 
 server '192.168.0.112', user: 'deployer', roles: %w{web app db}, port: 2247
 
@@ -26,11 +11,11 @@ server '192.168.0.112', user: 'deployer', roles: %w{web app db}, port: 2247
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+  set :ssh_options, {
+    keys: %w(/home/max/.ssh/id_rsa),
+    forward_agent: true,
+    auth_methods: %w(password)
+  }
 #
 # And/or per server (overrides global)
 # ------------------------------------
